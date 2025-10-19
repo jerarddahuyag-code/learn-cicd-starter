7c3b55b5dba6976397e37dfafcff90194e003020
jkdahuyag <jerard.dahuyag@gmail.com>
2025-10-19 21:29:52 +0800

    styling

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,28 +1,28 @@
 package auth
 
 import (
+	"errors"
+	"net/http"
 	"strings"
 	"testing"
-	"net/http"
-	"errors"
 )
 
-type returnValues struct{	
+type returnValues struct {
 	auth string
-	err error
+	err  error
 }
 
 func TestGetApiKey(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	tests := map[string]struct {
-		inputKey string
+		inputKey   string
 		inputValue string
-		want returnValues
+		want       returnValues
 	}{
-		"correct" : {inputKey: "Authorization", inputValue: "ApiKey eyJKekus", want: returnValues{ auth: "eyJKekus", err: nil}},
-		"noAuthHeader" : {inputKey: "none", inputValue: "ApiKey eyJKekus", want: returnValues{ auth: "", err: errors.New("no authorization header included")}},
-		"missingDirective" : {inputKey: "Authorization", inputValue: "ApiKey", want: returnValues{ auth: "", err: errors.New("malformed authorization header")}},
-		"wrongAuthType" : {inputKey: "Authorization", inputValue: "Basic etetet", want: returnValues{ auth: "", err: errors.New("malformed authorization header")}},
+		"correct":          {inputKey: "Authorization", inputValue: "ApiKey eyJKekus", want: returnValues{auth: "eyJKekus", err: nil}},
+		"noAuthHeader":     {inputKey: "none", inputValue: "ApiKey eyJKekus", want: returnValues{auth: "", err: errors.New("no authorization header included")}},
+		"missingDirective": {inputKey: "Authorization", inputValue: "ApiKey", want: returnValues{auth: "", err: errors.New("malformed authorization header")}},
+		"wrongAuthType":    {inputKey: "Authorization", inputValue: "Basic etetet", want: returnValues{auth: "", err: errors.New("malformed authorization header")}},
 	}
 	for name, testCase := range tests {
 		header := req.Header.Clone()
@@ -30,24 +30,24 @@ func TestGetApiKey(t *testing.T) {
 		got, apiErr := GetAPIKey(header)
 		gotStruct := returnValues{
 			auth: got,
-			err: apiErr,
+			err:  apiErr,
 		}
-		if !CompareHeaders(name, gotStruct, testCase.want, t){
+		if !CompareHeaders(name, gotStruct, testCase.want, t) {
 			t.Fatalf("%s: expcted: %v, %v; got: %v, %v", name, testCase.want.auth, testCase.want.err, got, apiErr)
 		}
 	}
 }
-func CompareHeaders(name string, got, want returnValues, t *testing.T) bool{
-	if want.err != nil{
-		if got.err == nil{
+func CompareHeaders(name string, got, want returnValues, t *testing.T) bool {
+	if want.err != nil {
+		if got.err == nil {
 			return false
 		}
-		if !strings.Contains(got.err.Error(), want.err.Error()){
+		if !strings.Contains(got.err.Error(), want.err.Error()) {
 			return false
 		}
 	}
 
-	if want.err == nil && got.err != nil{
+	if want.err == nil && got.err != nil {
 		return false
 	}
 
@@ -57,4 +57,3 @@ func CompareHeaders(name string, got, want returnValues, t *testing.T) bool{
 
 	return true
 }
-
